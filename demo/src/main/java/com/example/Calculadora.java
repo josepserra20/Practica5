2da433b7a8b25236ea857e95d834756191a1c9e4
josepserra20 <jserravives@cifpfbmoll.eu>
2022-05-14 11:48:22 +0200

    casos tests de calculadora

--- demo/src/main/java/com/example/Calculadora.java
+++ demo/src/main/java/com/example/Calculadora.java
@@ -2,24 +2,52 @@ package com.example;
 
 public class Calculadora implements ICalculadora {
 
+    
+    
+    /** 
+     * @param valor1
+     * @param valor2
+     * @return int
+     */
     @Override
     public int sumar(int valor1, int valor2) {
         
         return valor1 + valor2;
     }
 
+    
+    /** 
+     * @param valor1
+     * @param valor2
+     * @return int
+     */
     @Override
     public int restar(int valor1, int valor2) {
         
         return valor1 - valor2;
     }
 
+    
+    /** 
+     * @param valor1
+     * @param valor2
+     * @return int
+     */
     @Override
     public int dividir(int valor1, int valor2) {
-       
-        return valor1 / valor2;
+       if (valor2 == 0 ) {
+           return 0;
+       } else {
+           return valor1 / valor2;
+       }
     }
 
+    
+    /** 
+     * @param valor1
+     * @param valor2
+     * @return int
+     */
     @Override
     public int multiplicar(int valor1, int valor2) {
         
--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -1,12 +1,52 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class AppTest {
+
+    @After
+    public void main() {
+        App test = new App();
+        test.toString();
+    }
+    
+    Calculadora tests = new Calculadora();
+    
     @Test
-    public void firstTest() {
-        assertTrue(true);
+    public void sumarTest() {
+        
+        assertEquals(0, tests.sumar(0, 0), 30);
+        assertEquals(15, tests.sumar(10, 5), 30);
+        assertEquals(-5, tests.sumar(-10, 5), 30);
+    
+    }
+    @Test
+    public void restarTest() {
+        
+        assertEquals(0, tests.restar(0, 0), 30);
+        assertEquals(5, tests.restar(10, 5), 30);
+        assertEquals(-15, tests.restar(-10, 5), 30);
+    
+    }
+    @Test
+    public void dividirTest() {
+        
+        assertEquals(0, tests.dividir(0, 0), 30);
+        assertEquals(2, tests.dividir(10, 5), 30);
+        assertEquals(-2, tests.dividir(-10, 5), 30);
+        assertEquals(2.5, tests.dividir(20, 8), 30);
+        
+    
+    }
+    @Test
+    public void multiplicarTest() {
+        
+        assertEquals(0, tests.multiplicar(0, 0), 30);
+        assertEquals(50, tests.multiplicar(10, 5), 30);
+        assertEquals(-50, tests.multiplicar(-10, 5), 30);
+        assertEquals(-500, tests.multiplicar(-10, 5), 30);
     }
 }
